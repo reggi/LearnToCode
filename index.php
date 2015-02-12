69a8a80ab4d7628a69dba5d902da885fafabc100
reggi <thomas@reggi.com>
2015-02-12 00:12:44 -0500

    got rid of tabs in favor of spaces

--- index.php
+++ index.php
@@ -4,40 +4,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Simple Canvas Game</title>
+  <meta charset="utf-8">
+  <title>Simple Canvas Game</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-	<link rel="stylesheet" href="/style.css">
-	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <link rel="stylesheet" href="/style.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-	<script src="/script.js"></script>
+  <script src="/script.js"></script>
 </head>
 <body>
 
 <div id='gamecontainer'>
-	<script src="js/game.php<?php echo "?level=".$level ?>"></script>
+  <script src="js/game.php<?php echo "?level=".$level ?>"></script>
 </div>
 
 <div id="buttoncontainer">
-	<div id="btn_run" class="button">Run Code</div>
-	<div id="btn_run_disable" class="buttondisable">Run Code</div>
-	<div id="btn_reset" class="button">Reset Level</div>
-	<div id="btn_reset_disable" class="buttondisable">Reset Level</div>
+  <div id="btn_run" class="button">Run Code</div>
+  <div id="btn_run_disable" class="buttondisable">Run Code</div>
+  <div id="btn_reset" class="button">Reset Level</div>
+  <div id="btn_reset_disable" class="buttondisable">Reset Level</div>
 </div>
 
 <div class='linebreak'></div>
 
 <div id="codecontainer">
-	<p>Code</p>
-	<ul id="sortable" class="sortable asdfasdfasdf">
-	</ul>
+  <p>Code</p>
+  <ul id="sortable" class="sortable asdfasdfasdf">
+  </ul>
 </div>
 
 <div id="commandcontainer">
-	<p>Commands</p>
-	<ul id='sortable' class='sortable'>
-		<?php	display_levelcommands($level) ?>
-	</ul>
+  <p>Commands</p>
+  <ul id='sortable' class='sortable'>
+    <?php  display_levelcommands($level) ?>
+  </ul>
 </div>
 
 <?php if(isset($_GET['level']) && $_GET['level']>=4) display_variablecontainer() ?>
